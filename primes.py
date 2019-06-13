00571126f605a23e1232e37d243c32523d8a4687
Rishav Dhar <rishavdhar96@gmail.com>
2019-06-13 16:46:42 +0100

    Complete Prime tests

--- primes.py
+++ primes.py
@@ -1,19 +1,10 @@
 def is_prime(number):
-    """Return True if *number* is prime."""
-    for element in range(number):
+    '''Return True if *number* is prime.'''
+    if number <= 1:
+        return False
+
+    for element in range(2, number):
         if number % element == 0:
             return False
 
     return True
-
-def print_next_prime(number):
-    """Print the closest prime number larger than *number*."""
-    index = number
-    while True:
-        index += 1
-        if is_prime(index):
-            print("{}".format(index))
-
-#https://jeffknupp.com/blog/2013/12/09/improve-your-python-understanding-unit-testing/
-#https://repl.it/repls/SatisfiedKnownTwintext
-#http://pythontesting.net/framework/unittest/unittest-introduction/
