6b9ee812554e66483b7356d46204eb5fce1e71a6
Rishav Dhar <rishavdhar96@gmail.com>
2019-06-25 13:30:43 +0100

    demo

--- convert_test.py
+++ convert_test.py
@@ -3,7 +3,7 @@ from xml.dom.minidom import parseString
 import json
 import sys
 
-content = sys.stdin.read()
+CONTENT = sys.stdin.read()
 obj = json.loads(content, strict=True)
 xml = dicttoxml(obj, attr_type=False, root=False)
 dom = parseString(xml)
