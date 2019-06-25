2c5096fb75126b560ba6dc01ee49eea7843d859f
Rishav Dhar <rishavdhar96@gmail.com>
2019-06-25 13:02:10 +0100

    demo

--- test_factorials.py
+++ test_factorials.py
@@ -1,32 +1,33 @@
-# '''
-# Testing generation of factorials.
-# '''
+'''
+Testing generation of factorials.
+'''
 
-# import unittest
-# from factorials import fact, div
+import unittest
+from factorials import fact, div
 
 
-# class TestFactorial(unittest.TestCase):
-#     '''
-#     Our basic test class
-#     '''
+class TestFactorial(unittest.TestCase):
+    '''
+    Our basic test class
+    '''
 
-#     def test_fact(self):
-#         '''
-#         The actual test.
-#         Any method which starts with ``test_`` will be considered as a test
-#         case
-#         '''
+    def test_fact(self):
+        '''
+        The actual test.
+        Any method which starts with ``test_`` will be considered as a test
+        case
+        '''
 
-#         res = fact(5)
-#         self.assertEqual(res, 120)
+        res = fact(5)
+        self.assertEqual(res, 120)
 
-#     def test_error(self):
-#         '''
-#         To test exception raise due to run time error
-#         '''
+    def test_error(self):
+        '''
+        To test exception raise due to run time error
+        '''
 
-#         self.assertRaises(ZeroDivisionError, div, 0)
+        self.assertRaises(ZeroDivisionError, div, 0)
 
-# if __name__ == '__main__':
-#     unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
